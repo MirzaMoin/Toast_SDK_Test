176360de566b532e1c4cd0262ee9d127088e618f
DESKTOP-SRMLEJ6\admin <mirzamoinsuvya@gmail.com>
2021-03-08 18:30:32 +0530

    Activity Toaster Added

--- ToasterLibrary/src/main/java/com/example/toasterlibrary/ToasterActivity.java
+++ ToasterLibrary/src/main/java/com/example/toasterlibrary/ToasterActivity.java
@@ -2,14 +2,28 @@ package com.example.toasterlibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ToasterActivity extends AppCompatActivity {
 
+    Button btnOpen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toaster);
+
+        btnOpen = findViewById(R.id.btnOpen);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(ToasterActivity.this , SecondActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
