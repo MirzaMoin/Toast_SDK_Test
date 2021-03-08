4ea4e3a894b0a88f65138fc8a4379ba34083f20f
DESKTOP-SRMLEJ6\admin <mirzamoinsuvya@gmail.com>
2021-03-08 17:26:39 +0530

    Activity Toaster Added

--- app/src/main/java/com/example/toastsdktest/MainActivity.java
+++ app/src/main/java/com/example/toastsdktest/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.toastsdktest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
 import com.example.toasterlibrary.ToasterMessage;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ToasterMessage.s(this,"Hello World");
+
+
     }
 }
